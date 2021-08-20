64702918ea5a11249526c2e46a17568de7433ea5
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-08-20 16:26:37 +0600

    Checklist-5

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ for (const background of customBack) {
 
 // Checklist-3
 document.getElementById('add-list-btn').addEventListener('click', function () {
+    const listInputFiled = document.getElementById('list-item-filed');
     const listContainer = document.getElementById('list-container');
     const newElement = document.createElement('li');
-    newElement.innerText = 'New Item Added';
+    newElement.innerText = listInputFiled.value;
     listContainer.appendChild(newElement);
+    listInputFiled.value = '';
 });
 
 // Checklist-4
@@ -43,9 +45,39 @@ document.getElementById('btn-minus').addEventListener('click', function () {
     increaseDecrease(false);
 });
 
-// Cheklist-5
-// Problem in Checklist-5
-
 // Checklist 6,7
+
+// Checklist-5
+const plusButton = document.getElementById('challange-btn-plus');
+const minusButton = document.getElementById('challange-btn-minus');
+const inputFieldID = document.getElementById('challange-count-number');
+
+plusButton.addEventListener('click', function () {
+    const inputFiledValue = parseInt(inputFieldID.value);
+    if (inputFiledValue < 5) {
+        newInputValue = inputFiledValue + 1;
+        inputFieldID.value = newInputValue;
+
+        if (inputFiledValue == 4) {
+            plusButton.setAttribute('disabled', true);
+        }
+
+        else if (inputFiledValue != 4) {
+            plusButton.removeAttribute('disabled')
+        }
+    }
+});
+
+minusButton.addEventListener('click', function () {
+    const inputFiledValue = parseInt(inputFieldID.value);
+    if (inputFiledValue > 0) {
+        newInputValue = inputFiledValue - 1;
+        inputFieldID.value = newInputValue;
+    }
+
+    if (inputFiledValue == 1) {
+        minusButton.setAttribute('disabled', true);
+    }
+});
 
 
